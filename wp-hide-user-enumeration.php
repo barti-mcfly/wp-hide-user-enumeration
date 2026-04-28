24bc225b62283f918ede03a97a2e5ea93e48ec7c
barti-mcfly <bh@bartosz-behr.de>
2026-04-28 14:50:57 +0200

    Add files via upload
    v1.1.1

--- wp-hide-user-enumeration.php
+++ wp-hide-user-enumeration.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: WP Hide User Enumeration & Security
 Description: Blocks common WordPress paths for user enumeration, secures the REST API, disables user sitemaps, and protects against XML-RPC attacks. Includes GitHub updates.
-Version: 1.1.0
-Author: behrmedia
+Version: 1.1.1
+Author: barti-mcfly
 */
 
 if (!defined('ABSPATH')) {
